82a93ca257a622c7235a6590253e3f2b42683c0f
smithd23 <smithd23@greenhill.org>
2020-06-08 10:45:20 -0500

    uploading this i think?

---
+++ src/com/company/ReceiptVariable.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class ReceiptVariable {
+        public static void main(String[] args) {
+
+            // Calculate total owed, assuming 8% tax / 15% tip
+
+            System.out.println("Subtotal:");
+            int subtotal= 38 + 40+ 30;
+            System.out.println("Subtotal:"+ subtotal);
+            System.out.println("Tax:");
+            double tax= subtotal*.08;
+            System.out.println("Tax:"+ tax);
+            System.out.println("Tip:");
+            double tip= subtotal*.15;
+            System.out.println("Tip:"+ tip);
+            System.out.println("Total:");
+            double total= tax+tip+subtotal;
+            System.out.println("Total:"+ total);
+        }
+    }
+
