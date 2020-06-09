299d3ba67df930ac64661dcd2e7923190480cfc0
smithd23 <smithd23@greenhill.org>
2020-06-09 10:47:08 -0500

    commit and push but problems with large spaces

---
+++ src/com/company/Kshape.java
@@ -0,0 +1,35 @@
+package com.company;
+public class Kshape {
+    public static final int SIZE=8;
+    public static void main(String[] args) {
+        TopPart();
+        BottomPart();
+
+
+
+    }
+    public static void TopPart(){
+        char letter = 65;
+        for(byte count=SIZE; count<=SIZE; count--){
+            for (byte count2 = 0; count2 <= count; count2++) {
+                System.out.print((char) (letter + count2) + " ");
+
+            }
+            System.out.println("");
+
+
+        }
+
+    }
+    public static void BottomPart(){
+        char letter = 65;
+        for (byte count = 0; count <= SIZE; count++) {
+            for (byte count2 = 0; count2 <= count; count2++) {
+                System.out.print((char) (letter + count2) + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+
+}
