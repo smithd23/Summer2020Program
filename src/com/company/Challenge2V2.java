0721678f5bacc63913940f4b7dd0eeceac063f7e
smithd23 <smithd23@greenhill.org>
2020-06-09 09:18:12 -0500

    added size

--- src/com/company/Challenge2V2.java
+++ src/com/company/Challenge2V2.java
@@ -16,26 +16,26 @@ public class Challenge2V2 {
      // this is the method that creates the line.
     public static void Line() {
         System.out.print("+");
-        for(int dash=0; dash<9; dash++ ){
+        for(int dash=0; dash< 2*SIZE+1 ; dash++ ){
             System.out.print("-");
         }
         System.out.println("+");
     }
     public static void Small2Big() {
-        for (int line = 1; line <5; line++) {
+        for (int line = 1; line < SIZE+1; line++) {
             System.out.print("|");
-            for (int space = 1; space <= -1 * line + 5; space++){
+            for (int space = 1; space <= -1 * line + SIZE+1; space++){
                 System.out.print(" ");
             }
-            for (int slash = 1; slash <= 1 * line - 1; slash++){
+            for (int slash = 1; slash <=  line -1; slash++){
                 System.out.print("/");
             }
             System.out.print("*");
-            for (int backslash = 1; backslash <= 1 * line -1; backslash++){
+            for (int backslash = 1; backslash <= line -1; backslash++){
                 System.out.print("\\");
             }
 
-            for (int space = 1; space <= -1 * line + 5; space++) {
+            for (int space = 1; space <= -1 * line + SIZE + 1; space++) {
                 System.out.print(" ");
             }
 
@@ -46,19 +46,19 @@ public class Challenge2V2 {
 
         }
         public static void Big2Small(){
-            for (int line = 1; line <5; line++) {
+            for (int line = 1; line <SIZE + 1; line++) {
                 System.out.print("|");
-                for (int space = 1; space <= 1 * line + 0; space++) {
+                for (int space = 1; space <= line; space++) {
                     System.out.print(" ");
                 }
-                for (int backslash = 1; backslash <= -1 * line + 4; backslash++){
+                for (int backslash = 1; backslash <= -1 * line + SIZE; backslash++){
                     System.out.print("\\");
                 }
                 System.out.print("*");
-                for (int slash = 1; slash <= -1 * line + 4; slash++){
+                for (int slash = 1; slash <= -1 * line + SIZE; slash++){
                     System.out.print("/");
                 }
-                for (int space = 1; space <= 1 * line + 0; space++) {
+                for (int space = 1; space <= line; space++) {
                     System.out.print(" ");
                 }
                 System.out.println("|");
--- src/com/company/ReceiptVariableV2.java
+++ src/com/company/ReceiptVariableV2.java
@@ -1,5 +1,27 @@
 package com.company;
-//so this file will be like the other, except
+//so this file will be like the other, except i will try and make it broader so i can input different prices.
+// this is for fun and not an assignment
 public class ReceiptVariableV2 {
+    public static final double COST1=25;
+    public static final double COST2=41;
+    public static final double COST3=17;
+    public static void main(String[] args) {
+        //Daniel Smith
+
+        // Calculate total owed, assuming 8% tax / 15% tip
+
+
+        double subtotal= COST1 + COST2+ COST3;
+        System.out.println("Subtotal:"+ subtotal);
+
+        double tax= subtotal*.08;
+        System.out.println("Tax:"+ tax);
+
+        double tip= subtotal*.15;
+        System.out.println("Tip:"+ tip);
+
+        double total= tax+tip+subtotal;
+        System.out.println("Total:"+ total);
+    }
 
 }
